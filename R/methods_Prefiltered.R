d7ddbc8b69a829793ce56b436d12bf8443c45c6f
Dongjun Chung <dongjun.chung@gmail.com>
2017-02-23 16:30:44 -0500

    Initial commit
    PICS ver 0.0-1

---
+++ R/methods_Prefiltered.R
@@ -0,0 +1,24 @@
+#' @title Prefiltering summary
+#'
+#' @description Summary of prefiltering results.
+#'
+#' @rdname Prefiltered-class
+#' @aliases show,Prefiltered-method
+#'
+#' @param object output of prefilter function
+
+setMethod(
+  f="show",
+  signature="Prefiltered",
+  definition=function( object ) {
+
+    gene.before <- colnames(object@inputdata$data)
+    gene.after <- unique(unlist(sapply( object@xlist, colnames )))
+
+    cat( "Summary: Pre-filtering results (class: Prefiltered)\n" )
+    cat( "--------------------------------------------------\n" )
+    cat( "Number of genes before prefiltering: ", length(gene.before), "\n", sep="" )
+    cat( "Number of genes after prefiltering: ", length(gene.after), "\n", sep="" )
+    cat( "--------------------------------------------------\n" )
+  }
+)
